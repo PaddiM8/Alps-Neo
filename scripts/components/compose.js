31089ede576fab9fa8393cbfff95e9614d5a0af6
PaddiM8 <bakk@tuta.io>
2023-03-28 21:06:12 +0200

    Add save draft functionality

--- scripts/components/compose.js
+++ scripts/components/compose.js
@@ -4,19 +4,48 @@ import * as toast from "./toast";
 
 const composePane = document.getElementById("compose-pane");
 
-async function submit() {
+function showError() {
+    composePane.querySelector(".error").classList.remove("hidden");
+}
+
+function hideError() {
+    composePane.querySelector(".error").classList.add("hidden");
+}
+
+async function generateMessageId() {
+    const result = await fetch("/compose");
+    if (result.ok) {
+        return await result.text();
+    } else {
+        return null;
+    }
+}
+
+async function submit(isDraft = false) {
+    hideError();
+
     const to = multiInput.getValues(composePane.querySelector(".input-to"));
     const from = composePane.querySelector(".input-from").value;
     const cc = multiInput.getValues(composePane.querySelector(".input-cc"));
     const subject = composePane.querySelector(".input-subject").value;
     const message = composePane.querySelector(".input-message").value;
-    const messageId = await fetch("/compose");
 
     const formData = new FormData();
     formData.append("from", from);
     formData.append("to", to);
     formData.append("subject", subject);
     formData.append("text", message);
+
+    if (isDraft) {
+        formData.append("save_as_draft", "1");
+    }
+
+    const messageId = await generateMessageId();
+    if (!messageId) {
+        showError();
+        return;
+    }
+
     formData.append("message_id", messageId);
 
     const response = await fetch("/compose", {
@@ -26,13 +55,19 @@ async function submit() {
     });
 
     if (response.ok) {
+        if (isDraft) {
+            toast.show("Saved as draft.");
+        } else {
+            toast.show("Email was sent.");
+        }
+
         pane.close(composePane);
-        toast.show("Email was sent.");
     } else {
-        composePane.querySelector(".error").classList.remove("hidden");
+        showError();
     }
 }
 
-export function init() {
+export async function init() {
+    composePane.querySelector("button.as-draft").addEventListener("click", async () => await submit(true));
     composePane.querySelector("button.primary").addEventListener("click", submit);
 }
